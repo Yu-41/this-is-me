48a49c9357a7b9789057c54589f9f8f56813eda6
Yu-41 <hirosi41@gmail.com>
2020-05-26 10:26:47 +0900

    profile-table-created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  belong_to :profile
   validates :name, presence: true, uniqueness: true
-  
 end
