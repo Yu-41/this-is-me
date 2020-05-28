1c5e6b02ea41f0e2e4955684d87e1170d0c256a9
Yu-41 <hirosi41@gmail.com>
2020-05-28 17:27:10 +0900

    validate image added

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # belongs_to :profile
   validates :name, presence: true, uniqueness: true
+  validates :image, presence: true
 
   mount_uploader :image, ImageUploader
 end
