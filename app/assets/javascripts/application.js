3f15ee90c421850c56416b3b074435f99128aa52
Yu-41 <hirosi41@gmail.com>
2020-05-29 19:33:37 +0900

    tried to add copy url funtion

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,8 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require zeroclipboard
+
+$(document).ready(function() {
+  var clip = new ZeroClipboard($("#d_clip_button"))
+});
